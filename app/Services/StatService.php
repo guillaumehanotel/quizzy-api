dab583831220c6545dfad2300c7a4082ab74fdb0
Arnaud <arnaud@smartbottle.wine>
2019-12-05 17:33:43 +0100

    add new stats

--- app/Services/StatService.php
+++ app/Services/StatService.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Services;
 
+use App\Models\Genre;
 use Illuminate\Support\Facades\DB;
 
 class StatService {
@@ -23,23 +24,55 @@ class StatService {
             ->get();
     }
 
-    public function getAverageScore($games, $totalGames) {
+    public function getAverageAndBestScore($games, $totalGames) {
         $points = 0;
+        $bestScore = 0;
+
         foreach ($games as $game) {
             $points += $game->points;
+
+            if ($game->points > $bestScore) {
+                $bestScore = $game->points;
+            }
         }
-        return $points / $totalGames;
+        return [
+            'bestScore' => $bestScore,
+            'averageScore' => round($points / $totalGames)
+        ];
+    }
+
+    public function getFavoriteCategory($userId) {
+        $data = DB::table($this->table)
+            ->select(DB::raw('count(*) as counter'), 'genres.id', 'genres.name')
+            ->leftJoin('quizzs', 'quizzs.id', '=', $this->table . '.quizz_id')
+            ->leftJoin('genres', 'genres.id', '=', 'quizzs.genre_id')
+            ->where('user_id', '=', $userId)
+            ->groupBy('genres.id', 'genres.name')
+            ->get();
+
+        $max = 0;
+        $favoriteCategory = 0;
+        foreach ($data as $d) {
+            if ($d->counter > $max) {
+                $max = $d->counter;
+                $favoriteCategory = $d->id;
+            }
+        }
+        return Genre::find($favoriteCategory);
     }
 
     public function getStats($userId) {
         $games = $this->getTotalGames($userId);
         $totalGames = count($games);
+        $scores = $this->getAverageAndBestScore($games, $totalGames);
         $wineGames = $this->getWinGames($userId);
         $stats = [
             'games' => $games,
             'totalGames' => (int)$totalGames,
             'winGames' => (int)count($wineGames),
-            'averageScore' => (int)$this->getAverageScore($games, $totalGames)
+            'averageScore' => (int)$scores['averageScore'],
+            'bestScore' => (int)$scores['bestScore'],
+            'favoriteCategory' => $this->getFavoriteCategory($userId)
         ];
         return $stats;
     }
